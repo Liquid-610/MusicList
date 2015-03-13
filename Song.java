5dde9b3bc263b449eeeed57d2bb99dbd01c57f5f
Liquid-610 <jamesbarnes71@gmail.com>
2015-03-13 11:32:58 -0400

    still woring

--- Song.java
+++ Song.java
@@ -2,29 +2,17 @@ public class Song {
   
   public String artist;
   public String name;
-  // More fields here
-  public String score = "100";
- double scoreD = Double.parseDouble(scoreD); 
- 
- public String notes = "";
- public int year = 1997;
+  public int year;
+  public double score;
+  public String notes = "";
  
   // Default constructor.. you'll need to modify this
-  public Song(String artist, String name, String score, String notes, int year) { 
+  public Song(String artist, String name, int year, double score, String notes) { 
     this.artist = artist;
     this.name = name;
     this.score = score;
     this.year = year;
     this.notes = notes;
-    
-    Song artist = new Song();
-    Song name = new Song();
-    Song score = new Song();
-    Song year = new Song();
-    Song artist = new Song();
-    
-    
-    
   }
   
   /* ADD YOUR CODE HERE */
--- Song.java~
+++ Song.java~
@@ -3,13 +3,39 @@ public class Song {
   public String artist;
   public String name;
   // More fields here
-  
+  public String score = "100";
+ double scoreD = Double.parseDouble(scoreD); 
+ 
+ public String notes = "";
+ public int year = 1997;
+ 
   // Default constructor.. you'll need to modify this
-  public Song(String artist, String name) { 
+  public Song(String artist, String name, String score, String notes, int year) { 
     this.artist = artist;
     this.name = name;
+    this.score = score;
+    this.year = year;
+    this.notes = notes;
+    
+    Song artist = new Song();
+    Song name = new Song();
+    Song score = new Song();
+    Song year = new Song();
+    Song artist = new Song();
+    
+    
+    
   }
   
   /* ADD YOUR CODE HERE */
+  
+  
+
+  
+  
+  
+  
+  
+  
   
 }
